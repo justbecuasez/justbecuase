444e34b933804547c339431ba53557c266c95125
akashmahlaz <akashdalla408@gmail.com>
2025-12-07 00:55:41 +0530

    added new 277 in sign in 4 to 6

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -274,7 +274,7 @@ export default function SignUpPage() {
 
                   <div className="grid grid-cols-2 gap-4">
                     <Button variant="outline" type="button" className="w-full bg-transparent">
-                      <svg className="mr-2 h-4 w-4" viewBox="0 0 24 24">
+                      <svg className="mr-2 h-6 w-6" viewBox="0 0 24 24">
                         <path
                           fill="#4285F4"
                           d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"
